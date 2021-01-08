e52ae0f0bedfd4456a7b98aaa5ad878619e55998
Tom Goral <tg@tomgoral.com>
2021-01-07 21:19:15 -0500

    updated exercises

---
+++ m324/index.js
@@ -0,0 +1,28 @@
+/////////////////////////////////////////
+// Complex Data Types - Objects Part 1 //
+/////////////////////////////////////////
+
+// Objects
+
+// describe a peice of data in depth
+
+// key: value pairs
+var dog = {
+    name: "spot",
+    age: 4,
+    isHungry: true,
+    favToys: ["bone", "ball", "squeeky thing"]
+}
+
+// dot notation
+console.log(dog.age)
+
+// bracket notation
+console.log(dog["age"])
+
+var larry = {
+    name: "larry",
+    age: 20,
+    friends: ["john", "lisa", "al"],
+    isPetOwner: true
+}
